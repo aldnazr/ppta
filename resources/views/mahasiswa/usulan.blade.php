b45d25614741d17f17cb7ecf1965afd2e7690cff
anythinks <alauddinazhary05@gmail.com>
2025-01-23 13:51:32 +0700

    refactor usulan view for improved layout and readability

--- resources/views/mahasiswa/usulan.blade.php
+++ resources/views/mahasiswa/usulan.blade.php
@@ -14,8 +14,8 @@
                         class="p-4 border border-zinc-200 rounded-md lg:rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 cursor-pointer">
                         <h2 class="text-lg font-semibold text-blue-600">Judul TA: {{ $ta->judul }}</h2>
                         <p class="text-sm text-gray-600 mt-2"><strong>Pengusul:</strong> {{ $ta->pengusul }}</p>
-                        <p class="text-sm text-gray-600 mt-2"><strong>Deskripsi:</strong>
-                            {{ Str::limit($ta->deskripsi, 100) }}</p>
+                        <p class="text-sm text-gray-600 mt-2 line-clamp-1"><strong>Deskripsi:</strong>
+                            {{ $ta->deskripsi }}</p>
                     </li>
                 @endforeach
             </ul>
@@ -27,11 +27,24 @@
 
             <!-- Popup Window -->
             <x-popup-window>
-                <div class="flex flex-col">
-                    <strong class="text-gray-700 mb-0.5">Pengusul:</strong>
-                    <p class="mb-2" x-text="pengusul"></p>
-                    <strong class="text-gray-700 mb-0.5">Deskripsi:</strong>
-                    <p class="text-gray-700" x-text="description"></p>
+                <div class="flex flex-col space-y-4 text-sm md:text-base">
+                    <div class="grid grid-cols-12 gap-4">
+                        <div class="col-span-4 font-medium text-gray-600">
+                            <span class="block mb-1">Pengusul</span>
+                        </div>
+                        <div class="col-span-8 text-gray-800" x-text="pengusul">
+                            <!-- Nama Pengusul -->
+                        </div>
+                    </div>
+
+                    <div class="grid grid-cols-12 gap-4">
+                        <div class="col-span-4 font-medium text-gray-600">
+                            <span class="block mb-1">Deskripsi</span>
+                        </div>
+                        <div class="col-span-8 text-gray-800 leading-relaxed" x-text="description">
+                            <!-- Deskripsi Proposal -->
+                        </div>
+                    </div>
                 </div>
             </x-popup-window>
         </div>
