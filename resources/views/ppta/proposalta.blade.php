586ccbf27c3a73433d94c73b6f18fd0172ab073c
anythinks <alauddinazhary05@gmail.com>
2025-02-07 15:23:33 +0700

    refactor search bar components for improved styling and consistency across views

--- resources/views/ppta/proposalta.blade.php
+++ resources/views/ppta/proposalta.blade.php
@@ -31,16 +31,11 @@
                 </form>
                 <form id="searchForm" method="GET" action="{{ url()->current() }}" class="w-auto flex items-center">
                     <!-- Search Bar -->
-                    <div class="relative w-full md:w-72">
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            class="absolute h-full left-2 flex items-center h-4 w-4 text-gray-400" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
+                    <div class="relative flex w-full md:w-72 items-center">
+                        <i class="fa-solid fa-magnifying-glass fa-sm absolute left-3 text-gray-700/90"></i>
                         <input type="text" name="search" value="{{ request('search') }}"
-                            @keydown.enter="event.target.form.submit()" placeholder="Pencarian..."
-                            class="w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-normal duration-300 bg-gray-50 py-2 pl-8 pr-4 text-sm" />
+                            @keydown.enter="event.target.form.submit()" placeholder="Pencarian"
+                            class="w-full py-2 pl-10 pr-4 bg-gray-100 placeholder-gray-500 text-sm text-gray-700 rounded-lg ring ring-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300" />
                     </div>
                 </form>
             </div>
